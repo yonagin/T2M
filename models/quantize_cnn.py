3b6b0dfb83f0884c2c90e05952bc2f01e9702579
yonagin <3118391854@qq.com>
2026-03-01 23:09:13 +0800

    init

--- models/quantize_cnn.py
+++ models/quantize_cnn.py
@@ -15,7 +15,7 @@ class QuantizeEMAReset(nn.Module):
         self.init = False
         self.code_sum = None
         self.code_count = None
-        self.register_buffer('codebook', torch.zeros(self.nb_code, self.code_dim).cuda())
+        self.register_buffer('codebook', torch.zeros(self.nb_code, self.code_dim))
 
     def _tile(self, x):
         nb_code_x, code_dim = x.shape
@@ -310,7 +310,7 @@ class QuantizeEMA(nn.Module):
         self.init = False
         self.code_sum = None
         self.code_count = None
-        self.register_buffer('codebook', torch.zeros(self.nb_code, self.code_dim).cuda())
+        self.register_buffer('codebook', torch.zeros(self.nb_code, self.code_dim))
 
     def _tile(self, x):
         nb_code_x, code_dim = x.shape
